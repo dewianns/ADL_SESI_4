b473c49ad4abecf6937c6df2a2115f1efe60f066
dewianns <anam.dewiannisa@gmail.com>
2021-10-13 07:39:17 +0700

    DewiAnnisaAnam_Ujian4

--- ADL_BOOTHCAMP_DAY4/src/com/adl/boothcamp/Github.java
+++ ADL_BOOTHCAMP_DAY4/src/com/adl/boothcamp/Github.java
@@ -9,7 +9,7 @@ public class Github {
 		//int [] myarray; //deklarasi variable hanya size array nya saja tanpa isi
 		//myarray = new int[10]; //deklarasi dengan isi dari array
 		Github gth = new Github();
-		gth.latihan3();
+		gth.insertData();
 		//int myarray1[] = {3,4,2,5,1,6,7,8,9};
 		//myarray[0] = 1;
 		//myarray[4] = 1;
@@ -64,12 +64,48 @@ public class Github {
 		}
 	}
 
-	public static void latihan3() {
+	//public static void latihan3() {
+		//Scanner scan = new Scanner(System.in);
+		
+		//System.out.println("Jumlah data : ");
+		//int data =  scan.nextInt();
+		//Int [] arr = new String[data];
+		
+	//}
+
+	public void insertData() {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Jumlah data : ");
-		int data =  scan.nextInt();
-		Int [] arr = new String[data];
+		System.out.println("Masukkan banyak data : ");
 		
+		int jmlData = scan.nextInt();
+		 ModelData[] data = new ModelData[jmlData];
+		 
+		 for (int i = 0; i < jmlData; i++) {
+			 data[i] = new ModelData();
+			 System.out.println("Masukkan nama anda : ");
+			 String nama = scan.next();
+			 data[i].setNama(nama);
+			 
+			 System.out.println("Masukkan alamat anda : ");
+			 String alamat = scan.next();
+			 data[i].setAlamat(alamat);
+		 }
+		 
+		 //cara for each
+		 for (ModelData modelData : data) {
+			 System.out.println("==============================");
+			 System.out.println("Nama :" + modelData.getNama());
+			 System.out.println("Alamat :" + modelData.getAlamat());
+			 System.out.println("==============================");
+		 }
+		 
+		 //cara for next
+		 for (int i = 0; i < jmlData; i++) {
+			 System.out.println("==============================");
+			 System.out.println("Nama :" + data[i].getNama());
+			 System.out.println("Alamat :" + data[i].getAlamat());
+			 System.out.println("==============================");
+		 }
 	}
 }
---
+++ ADL_BOOTHCAMP_DAY4/src/com/adl/boothcamp/ModelData.java
@@ -0,0 +1,23 @@
+package com.adl.boothcamp;
+
+public class ModelData {
+
+		private String nama;
+		private String alamat;
+		
+		
+		
+		public String getNama() {
+			return nama;
+		}
+		public void setNama(String nama) {
+			this.nama = nama;
+		}
+		public String getAlamat() {
+			return nama;
+		}
+		public void setAlamat(String alamat) {
+			this.alamat = alamat;
+		}
+
+}
